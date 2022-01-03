97db6d5a64cf2011744e294434be12da462b2452
Zavier Thompson <Zayveeeee@Zaviers-MBP.lan1>
2022-01-03 00:02:05 -0500

    python prework question 1

---
+++ preworkquestions.py
@@ -0,0 +1,11 @@
+# Question 1:
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the fun
+
+def hello_name(user_name):
+    """Display a simple greeting."""
+    print("hello_ " + user_name.title() + "!")
+
+    hello_name("USERNAME")
+    
+
+
